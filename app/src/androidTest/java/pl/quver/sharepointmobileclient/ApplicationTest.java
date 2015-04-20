42f7268a27daf44ca8fef1e5942c36b1abf4c96b
Pawel Bednorz <pawel@quver.pl>
2015-04-20 02:39:47 +0200

    Simple test case

--- app/src/androidTest/java/pl/quver/sharepointmobileclient/ApplicationTest.java
+++ app/src/androidTest/java/pl/quver/sharepointmobileclient/ApplicationTest.java
@@ -3,10 +3,56 @@ package pl.quver.sharepointmobileclient;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
+import com.google.gson.Gson;
+
+import junit.framework.Assert;
+
+import pl.quver.sharepointmobileclient.rest.models.TaskEntity;
+
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    class WrongEntity{
+
+    }
+
+    private String mFullJSON;
+    private String mCorruptedJSON;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        mFullJSON = "{ \"Id\" : 1, \"Title\" : \"Tomasz\"}";
+
+        mCorruptedJSON = "{ \"Id\" : 1, \"Title\" : \"Tomasz\",";
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        mCorruptedJSON = null;
+        mFullJSON = null;
+
+        super.tearDown();
+    }
+
+    public void testMapFull() throws Exception {
+        try {
+            TaskEntity testFull = new Gson().fromJson(mFullJSON, TaskEntity.class);
+            Assert.assertEquals("Tomasz", testFull.getmTitle());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testMapCorrupted() throws Exception {
+        try {
+            TaskEntity testCorrupted = new Gson().fromJson(mCorruptedJSON, TaskEntity.class);
+            Assert.assertNull(testCorrupted.getmTitle());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public ApplicationTest() {
         super(Application.class);
     }
